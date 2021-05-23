0dcbee73605d73f6cfcae6a1cf7dbaf79071018f
Anwesh43 <anweshthecool0@gmail.com>
2021-05-24 02:43:54 +0530

    adding renderer in view

--- cuesquareview/src/main/java/com/example/cuesquareview/CueSquareView.kt
+++ cuesquareview/src/main/java/com/example/cuesquareview/CueSquareView.kt
@@ -60,14 +60,16 @@ fun Canvas.drawCSNode(i : Int, scale : Float, paint : Paint) {
 
 class CueSquareView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
@@ -191,7 +193,7 @@ class CueSquareView(ctx : Context) : View(ctx) {
         private val cs : CueSquare = CueSquare(0)
         private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
-        fun render(canvas : Canvas, paint : Paint) {
+        fun render(canvas : Canvas) {
             canvas.drawColor(backColor)
             cs.draw(canvas, paint)
             animator.animate {
