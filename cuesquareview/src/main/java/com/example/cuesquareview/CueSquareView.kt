0fefe9ef438fcd3712cc5be2d60c8ad5745dbad2
Anwesh43 <anweshthecool0@gmail.com>
2021-05-24 02:25:38 +0530

    adding Canvas extension methods

--- cuesquareview/src/main/java/com/example/cuesquareview/CueSquareView.kt
+++ cuesquareview/src/main/java/com/example/cuesquareview/CueSquareView.kt
@@ -29,3 +29,31 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawCueSquare(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sf: Float = scale.sinify()
+    val sf1: Float = sf.divideScale(0, parts)
+    val sf2: Float = sf.divideScale(1, parts)
+    val sf3: Float = sf.divideScale(2, parts)
+    val sf4: Float = sf.divideScale(3, parts)
+    val r: Float = Math.min(w, h) / rFactor
+    val barW : Float = Math.min(w, h) / barWFactor
+    save()
+    save()
+    translate(w / 2, 0f)
+    drawRect(RectF(-barW / 2, -h * 0.5f * sf1, barW / 2, 0f), paint)
+    restore()
+    save()
+    translate(0f, h / 2 - r)
+    drawRect(RectF(0f, -barW / 2, w * 0.5f * sf3,barW / 2), paint)
+    restore()
+    drawCircle(w / 2 + (w / 2 + r) * sf4, -r + (h /2) * sf2, r, paint)
+    restore()
+}
+
+fun Canvas.drawCSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawCueSquare(scale, w, h, paint)
+}
