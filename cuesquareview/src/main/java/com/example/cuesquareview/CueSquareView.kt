f87e57c8fae1ec63dfa68c08ea6e98298f064399
Anwesh43 <anweshthecool0@gmail.com>
2021-05-24 02:16:25 +0530

    adding constants and libraries

--- cuesquareview/src/main/java/com/example/cuesquareview/CueSquareView.kt
+++ cuesquareview/src/main/java/com/example/cuesquareview/CueSquareView.kt
@@ -1,1 +1,27 @@
 package com.example.cuesquareview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.graphics.Canvas
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#311B92",
+    "#00C853",
+    "#00C853",
+    "#C51162"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val barWFactor : Float = 13.2f
+val rFactor : Float = 9.2f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+
