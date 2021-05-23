1f27ff576e6bda71bb00d4e15692738571699ae4
Anwesh43 <anweshthecool0@gmail.com>
2021-05-24 02:30:57 +0530

    adding CueSquareView class

--- cuesquareview/src/main/java/com/example/cuesquareview/CueSquareView.kt
+++ cuesquareview/src/main/java/com/example/cuesquareview/CueSquareView.kt
@@ -57,3 +57,19 @@ fun Canvas.drawCSNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawCueSquare(scale, w, h, paint)
 }
+
+class CueSquareView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
