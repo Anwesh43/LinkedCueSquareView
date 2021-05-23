d05d127150b83a6ccc95399e01c078fcd21d8c27
Anwesh43 <anweshthecool0@gmail.com>
2021-05-24 02:51:14 +0530

    adding view in activity and fullscreen support

--- app/src/main/java/com/example/linkedcuesquareview/MainActivity.kt
+++ app/src/main/java/com/example/linkedcuesquareview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.linkedcuesquareview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.cuesquareview.CueSquareView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        CueSquareView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
 
